073ba1a33d6b202214bc301749a03f7ac4b81926
Alejosd <alejosd5@gmail.com>
2015-05-24 11:48:10 -0500

    Especificación de libreria redstone en el archivo pubspec.yaml. Importación y creación de un servidor HTTP básico con redstone corriendo sobre el puerto 8080

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # NoteDart
 
-A sample command-line application.
+Ejemplo de implementación de una app web que crea, lee, actualiza y elimina notas.
+Construida con polymer del lado del cliente, redstone del lado del servidor y como BD MongoDB.
--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,13 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Alejandro Sierra Duran. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:NoteDart/NoteDart.dart' as NoteDart;
+import 'package:redstone/server.dart' as app;
 
-main(List<String> arguments) {
-  print('Hello world: ${NoteDart.calculate()}!');
+@app.Route("/")
+helloWorld() => "Hello, World!";
+
+main() {
+  app.setupConsoleLog();
+  app.start();
 }
